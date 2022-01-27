4b8e073725dd2a821e507f8b5833cacba9e213f8
alan3737 <alanzheng2323@gmail.com>
2022-01-27 11:48:46 -0500

    finished the project

--- js/main.js
+++ js/main.js
@@ -11,9 +11,10 @@ function computerPlay() {
 }
 
 function play(playerSel, computerSel) {
+    
     playerSel = playerSel.toLowerCase();
     if (playerSel === computerSel) {
-        return "It's tie"
+        return "It's a tie"
     }
     switch (true) {
         case playerSel === 'rock' && computerSel === 'scissors':
@@ -28,25 +29,36 @@ function play(playerSel, computerSel) {
 
 }
 
-function game() {
-    let p_s = 0
-    let c_s = 0
-    for (let i = 0; i < 2; i++) {
-        let result = play(prompt("choose"), computerPlay()).slice(0, 7)
-        if (result === 'You Win') {
-            p_s++
-        } else if(result ==="It's a ti"){
-            continue
-        } else {
-           c_s++ 
-        }
-    }
-    if(p_s === c_s) {
-        console.log("It's a Tie")
-    } else if(p_s > c_s) {
-        console.log("You win!") 
+function game(e) {
+    let p_s = document.querySelector(".player .scr");
+    let c_s = document.querySelector(".computer .scr");
+    
+    let result = play(this.id , computerPlay()).slice(0, 5)
+    console.log(result);
+    if (result === 'You W') {
+        p_s.textContent = +p_s.textContent + 1;
+    } else if(result ==="You L"){
+        c_s.textContent = +c_s.textContent + 1;
     } else {
-        console.log("You lose!")
+        p_s.textContent = +p_s.textContent + 1;
+        c_s.textContent = +c_s.textContent + 1;
     }
+    if (+p_s.textContent >= 5 || +c_s.textContent >= 5) {
+        const winner = document.querySelector(".title")
+        if(p_s.textContent === c_s.textContent) {
+            winner.textContent = "It's a Tie";
+        } else if(p_s.textContent > c_s.textContent) {
+            winner.textContent = "You Win!";
+        } else {
+            winner.textContent = "You Lose!";
+        }
+        const rm_choice = document.querySelectorAll(".move")
+        rm_choice.forEach(rm => rm.removeEventListener("click", game))
+    }
+
 }
 
+
+
+const choices = document.querySelectorAll(".move");
+choices.forEach(choice => choice.addEventListener("click", game))
